f835c97313e700898ca483d25cb8957d02897207
habebe <habebe@objectivity.com>
2013-01-21 02:43:42 -0600

    error catching.

--- benchmark/scripts/operations/graph_v_search.py
+++ benchmark/scripts/operations/graph_v_search.py
@@ -62,6 +62,19 @@ class operation(db_benchmark.operation):
         self.run_operation()
         pass
 
+    def os_remove_profile_files(self):
+        listing = os.listdir(".")
+        for i in listing:
+            if i.endswith(".profile"):
+                try:
+                    os.remove(i)
+                except Exception as e:
+                    print e
+                    pass
+                pass
+            pass
+        pass
+
     def run_operation(self):
         random.seed(self.seed)
         for _index in self.index: 
@@ -104,62 +117,73 @@ class operation(db_benchmark.operation):
                                     if self.case_object:
                                         if os.path.exists(profileName):
                                             f = file(profileName,"r")
-                                            line = f.read()
-                                            data = eval(line)
-                                            if (_v_size == data["opsize"]):
-                                                platform_object = self.db.create_unique_object(db_objects.model.platform,"name",data["os"])
-                                                index_object = self.db.create_unique_object(db_objects.model.index_type,"name",_index)
-                                                
-                                                assert(_v_size == data["opsize"])
-                                                case_data_object = self.db.create_object(db_objects.model.case_data,
-                                                                                         timestamp=self.db.now_string(True),
-                                                                                         case_id=self.case_object.id,
-                                                                                         tag_id=self.tag_object.id,
-                                                                                         engine_id=engine.id,
-                                                                                         size=data["size"],
-                                                                                         time=data["time"],
-                                                                                         memory_init=data["mem_init"],
-                                                                                         memory_used=data["mem_used"],
-                                                                                         memory_committed=data["mem_committed"],
-                                                                                         memory_max=data["mem_max"],
-                                                                                         op_size=data["opsize"],
-                                                                                         rate=data["rate"],
-                                                                                         page_size=_page_size,
-                                                                                         cache_init=self.propertyFile.getInitCache(),
-                                                                                         cache_max=self.propertyFile.getMaxCache(),
-                                                                                         tx_size=_txsize,
-                                                                                         platform_id=platform_object.id,
-                                                                                         threads=_threads,
-                                                                                         index_id=index_object.id,
-                                                                                         status=1
-                                                                                         )
-                                                if _v_size_set != self.graph_size:
-                                                    case_data_object.setDataValue("search_set_size",_v_size_set)
-                                                    self.db.update(case_data_object)
+                                            line = f.readline()
+                                            data = None
+                                            try:
+                                                data = eval(line)
+                                            except Exception as e:
+                                                print e
+                                                data = None
+                                                self.os_remove_profile_files()
+                                                #os.remove(profileName)
+                                                pass
+                                            if data != None:
+                                                if (_v_size == data["opsize"]):
+                                                    platform_object = self.db.create_unique_object(db_objects.model.platform,"name",data["os"])
+                                                    index_object = self.db.create_unique_object(db_objects.model.index_type,"name",_index)
+                                                    
+                                                    assert(_v_size == data["opsize"])
+                                                    case_data_object = self.db.create_object(db_objects.model.case_data,
+                                                                                             timestamp=self.db.now_string(True),
+                                                                                             case_id=self.case_object.id,
+                                                                                             tag_id=self.tag_object.id,
+                                                                                             engine_id=engine.id,
+                                                                                             size=data["size"],
+                                                                                             time=data["time"],
+                                                                                             memory_init=data["mem_init"],
+                                                                                             memory_used=data["mem_used"],
+                                                                                             memory_committed=data["mem_committed"],
+                                                                                             memory_max=data["mem_max"],
+                                                                                             op_size=data["opsize"],
+                                                                                             rate=data["rate"],
+                                                                                             page_size=_page_size,
+                                                                                             cache_init=self.propertyFile.getInitCache(),
+                                                                                             cache_max=self.propertyFile.getMaxCache(),
+                                                                                             tx_size=_txsize,
+                                                                                             platform_id=platform_object.id,
+                                                                                             threads=_threads,
+                                                                                             index_id=index_object.id,
+                                                                                             status=1
+                                                                                             )
+                                                    if _v_size_set != self.graph_size:
+                                                        case_data_object.setDataValue("search_set_size",_v_size_set)
+                                                        self.db.update(case_data_object)
+                                                        pass
+                                                    case_data_key = case_data_object.generateKey()
+                                                    case_data_stat_object = self.db.fetch_using_generic(db_objects.model.case_data_stat,
+                                                                                                        key=case_data_key,
+                                                                                                        case_id=self.case_object.id
+                                                                                                        )
+                                                    if (len(case_data_stat_object) == 0):
+                                                        case_data_stat_object = self.db.create_unique_object(db_objects.model.case_data_stat,
+                                                                                                             "key",case_data_key,
+                                                                                                             case_id=self.case_object.id
+                                                                                                             )
+                                                    else:
+                                                        case_data_stat_object = case_data_stat_object[0]
+                                                        pass
+                                                    case_data_stat_object.addCounter()
+                                                    case_data_stat_object.setRateStat(data["rate"])
+                                                    case_data_stat_object.setTimeStat(data["time"])
+                                                    case_data_stat_object.setMemInitStat(data["mem_init"])
+                                                    case_data_stat_object.setMemUsedStat(data["mem_used"])
+                                                    case_data_stat_object.setMemCommittedStat(data["mem_committed"])
+                                                    case_data_stat_object.setMemMaxStat(data["mem_max"])
+                                                    self.db.update(case_data_stat_object)
+                                                    f.close()
+                                                    self.os_remove_profile_files()
+                                                    #os.remove(profileName)
                                                     pass
-                                                case_data_key = case_data_object.generateKey()
-                                                case_data_stat_object = self.db.fetch_using_generic(db_objects.model.case_data_stat,
-                                                                                                    key=case_data_key,
-                                                                                                    case_id=self.case_object.id
-                                                                                                    )
-                                                if (len(case_data_stat_object) == 0):
-                                                    case_data_stat_object = self.db.create_unique_object(db_objects.model.case_data_stat,
-                                                                                                         "key",case_data_key,
-                                                                                                         case_id=self.case_object.id
-                                                                                                         )
-                                                else:
-                                                    case_data_stat_object = case_data_stat_object[0]
-                                                    pass
-                                                case_data_stat_object.addCounter()
-                                                case_data_stat_object.setRateStat(data["rate"])
-                                                case_data_stat_object.setTimeStat(data["time"])
-                                                case_data_stat_object.setMemInitStat(data["mem_init"])
-                                                case_data_stat_object.setMemUsedStat(data["mem_used"])
-                                                case_data_stat_object.setMemCommittedStat(data["mem_committed"])
-                                                case_data_stat_object.setMemMaxStat(data["mem_max"])
-                                                self.db.update(case_data_stat_object)
-                                                f.close()
-                                                os.remove(profileName)
                                                 pass
                                             pass
                                         pass
