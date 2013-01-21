ae6bcc679fce14120f61f48896d2b71dfb7164c4
habebe <habebe@objectivity.com>
2013-01-21 05:10:54 -0600

    corrected table format.

--- test_suites/vertex_operations/f_disk/__suite__.py
+++ test_suites/vertex_operations/f_disk/__suite__.py
@@ -24,6 +24,33 @@ plot_view = {
         ]
     }
 
+
+
+search_table_view = [
+    [{"sTitle":"Database engine"},{"content":"object.engine()"}],
+    [{"sTitle":"Disks"},{"content":"object.object_data('diskmap','n/a')"}],
+    [{"sTitle":"Threads"},{"content":"object.threads()"}],
+    [{"sTitle":"Search Size"},{"content":"object.op_size()"}],
+    [{"sTitle":"Index Type"},{"content":"object.index_type()"}],
+    [{"sTitle":"Rate (v/s)"},{"content":"'%.2f'%(object.rate_avg())"}],
+    [{"sTitle":"Heap Memory (MB)"},{"content":"'%.3f'%(object.memory_used_avg()*1e-6)"}],
+    ]
+
+search_plot_view = {
+    "plot":[
+        {"name":"rate","data":("object.rate_avg()","object.threads()"),"xaxis":"Threads"},
+        {"name":"memory","data":("object.memory_used_avg()*1e-6","object.threads()"),"xaxis":"Threads"},
+        ],
+    "ivar":[
+        {"name":"Database engine","id":"object.engine_id()","content":"object.engine()"},
+        {"name":"Platform","id":"object.platform_id()","content":"object.platform()"},
+        {"name":"Index Type","id":"object.index_type_id()","content":"object.index_type()"},
+        {"name":"Disks","id":"object.object_data('diskmap',0)","content":"object.object_data('diskmap','n/a')"},
+        {"name":"Search Size","id":"object.op_size()","content":"object.op_size()"},
+        {"name":"Cache (initial,max) (MB)","id":"object.cache_max()","content":"'(%.0f,%.0f)'%(object.cache_init()*1e-3,object.cache_max()*1e-3)"},
+        ]
+    }
+
 cases = []
 
 for diskmap in [1,2,3,4]:
@@ -87,7 +114,7 @@ for diskmap in [1,2,3,4]:
                     "diskmap":diskmap,
                     "graph_size":[pow(2,20)]
                     },
-                "table_view":table_view,
-                "plot_view":plot_view
+                "table_view":search_table_view,
+                "plot_view":search_plot_view
                 }
             )
